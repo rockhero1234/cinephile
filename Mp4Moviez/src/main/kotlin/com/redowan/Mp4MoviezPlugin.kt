33aadda879dc3e343f281ac515869db95cfc021f
rockhero1234 <techartistdilip@gmail.com>
2025-11-22 13:58:30 +0530

    new provider

---
+++ Mp4Moviez/src/main/kotlin/com/redowan/Mp4MoviezPlugin.kt
@@ -0,0 +1,13 @@
+package com.redowan
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class Mp4MoviezPlugin : Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(Mp4MoviezProvider())
+    }
+}
